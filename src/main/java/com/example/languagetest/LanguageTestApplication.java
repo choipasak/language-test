d74727ac365083c2d159ae191ba3890d398bdbf6
Jeonghyun <chlwjdgus822@gmail.com>
2024-03-29 19:36:54 +0900

    first commit

---
+++ src/main/java/com/example/languagetest/LanguageTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.languagetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/languagetest/LanguageTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.languagetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
